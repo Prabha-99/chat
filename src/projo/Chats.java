e2c6e1151d4411bc04d5f560e3ecc5c84c5d7f83
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-03-08 11:33:43 +0530

    initial commit

---
+++ src/projo/Chats.java
@@ -0,0 +1,64 @@
+package projo;
+// Generated Mar 8, 2023 11:23:32 AM by Hibernate Tools 4.3.1
+
+
+import java.util.Date;
+
+
+public class Chats  implements java.io.Serializable {
+
+
+     private String chatId;
+     private String chatLink;
+     private Date startTime;
+     private Date endTime;
+
+    public Chats() {
+    }
+
+	
+    public Chats(String chatId) {
+        this.chatId = chatId;
+    }
+    public Chats(String chatId, String chatLink, Date startTime, Date endTime) {
+       this.chatId = chatId;
+       this.chatLink = chatLink;
+       this.startTime = startTime;
+       this.endTime = endTime;
+    }
+   
+    public String getChatId() {
+        return this.chatId;
+    }
+    
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+    public String getChatLink() {
+        return this.chatLink;
+    }
+    
+    public void setChatLink(String chatLink) {
+        this.chatLink = chatLink;
+    }
+    public Date getStartTime() {
+        return this.startTime;
+    }
+    
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+    public Date getEndTime() {
+        return this.endTime;
+    }
+    
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+
+
+
+}
+
+
---
+++ src/projo/Users.java
@@ -0,0 +1,81 @@
+package projo;
+// Generated Mar 8, 2023 11:23:32 AM by Hibernate Tools 4.3.1
+
+
+
+
+public class Users  implements java.io.Serializable {
+
+
+     private String userId;
+     private String userName;
+     private String email;
+     private String nickName;
+     private String password;
+     private String profileImage;
+
+    public Users() {
+    }
+
+	
+    public Users(String userId) {
+        this.userId = userId;
+    }
+    public Users(String userId, String userName, String email, String nickName, String password, String profileImage) {
+       this.userId = userId;
+       this.userName = userName;
+       this.email = email;
+       this.nickName = nickName;
+       this.password = password;
+       this.profileImage = profileImage;
+    }
+   
+    public String getUserId() {
+        return this.userId;
+    }
+    
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getUserName() {
+        return this.userName;
+    }
+    
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getNickName() {
+        return this.nickName;
+    }
+    
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getProfileImage() {
+        return this.profileImage;
+    }
+    
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+
+
+
+}
+
+
